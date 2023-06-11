635237df84bb46ee39f569f0bae468c480bd5fb1
mcdott <111994803+mcdott@users.noreply.github.com>
2023-06-11 15:07:47 -0400

    Synchronizes photo position for data capture and data visualization

--- app.py
+++ app.py
@@ -8,9 +8,34 @@ CORS(app)
 @app.route('/save', methods=['POST'])
 def save():
     data = request.get_json()
-    with open('Documents/dev/4310DataViz/data-vis-1/mouse_positions.json', 'w') as file:
+    with open('mouse_positions.json', 'w') as file:
         json.dump(data, file)
     return 'Data saved successfully.'
 
 if __name__ == '__main__':
     app.run(port=5000)
+
+# For 1st visualisation
+# Qs re. visual-motor development:
+# - How well can student fixate on a target?
+
+# Qs re. cognitive development:
+# - Is student's gaze drawn to the face and expression?
+# - Is student's gaze drawn to the hands and gestures?
+# - Does student's gaze shift back and forth between two interacting targets?
+
+# For 2nd visualisation show how these change over time by comparing to a baseline initial assessment:
+# Qs re. visual development:
+# - Does visual contrast level affect visual engagement?
+# - Does processing time affect visual engagement?
+# - Does visual complexity affect visual engagement?
+# - Does visual clutter affect visual engagement?
+# - Does visual salience affect visual engagement?
+# - Does visual familiarity affect visual engagement? (e.g. familiar faces)
+
+# Qs re. other factors:
+# - Time of day
+# - Elapsed time in session
+# - Environment (e.g. home, classroom, solo workspace)
+# - Environmental factors (e.g. noise, lighting, distractions)
+# - Facilitator (e.g. parent, teacher, EA, peer, none)
